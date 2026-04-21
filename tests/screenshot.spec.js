63dfffaad11c9e4c77eb9af9613fd8c317a3bf6f
mdemonmia <emonrpi@gmail.com>
2026-04-21 17:39:05 +0600

    fix screenshot test

--- tests/screenshot.spec.js
+++ tests/screenshot.spec.js
@@ -6,7 +6,7 @@ test('single page screenshot',async({page})=>{
 
 test('full page screenshot',async({page})=>{
     await page.goto('https://demoblaze.com/index.html')
-    await page.screenshot({path: 'tests/screenshots/'+Date.now()+'fullpage.png', fullpage:'true'})
+    await page.screenshot({path: 'tests/screenshots/'+Date.now()+'fullpage.png', fullPage:true})
 })
 
 test('element page screenshot',async({page})=>{
@@ -14,7 +14,8 @@ test('element page screenshot',async({page})=>{
     await page.locator('#login2').click()
     await page.locator('#loginusername').fill('Emon')
     await page.locator('#loginpassword').fill('test@123')
-    await page.locator("//button[normalize-space()='Log in']").click()
-    await page.waitForTimeout(2000);
     await page.locator("//*[@id='logInModal']/div").screenshot({path: 'tests/screenshots/'+Date.now()+'element.png'})
+    await page.locator("//button[normalize-space()='Log in']").click()
+
+    
 })
