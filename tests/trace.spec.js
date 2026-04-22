9cd1654ec547ae12ad7e5f384885d2dba233c652
mdemonmia <emonrpi@gmail.com>
2026-04-22 12:56:23 +0600

    trace file added

---
+++ tests/trace.spec.js
@@ -0,0 +1,9 @@
+const{test,expect}=require('@playwright/test')  
+test('trace view',async({page})=>{
+    await page.goto('https://demoblaze.com/index.html');
+    await page.getByRole('link',{name:'Log in'}).click();
+    await page.locator('#loginusername').fill('Emon');
+    await page.locator('#loginpassword').fill('test@123');
+    await page.getByRole('button',{name: 'Log in'}).click();
+    await expect(page.getByRole('link',{name:'Log out'})).toBeVisible();
+})
